6f742e6ddd9404b3b9e1fb377646be64bcb618f4
João Paulo <joaopptt@hotmail.com>
2024-08-15 21:50:08 -0300

    refactor: change to Hello, GCP

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import * as functions from "@google-cloud/functions-framework";
 functions.http(
   "helloWorld",
   (req: functions.Request, res: functions.Response) => {
-    res.send("Hello, world!");
+    res.send("Hello, GCP!");
   }
 );
